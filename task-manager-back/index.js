be3d77641c5b395fb105c224b3e44fa65c2f8019
Elian Emir Hadad <hadad.elian@gmail.com>
2023-10-13 05:34:30 -0300

    TODO: socket io

--- task-manager-back/index.js
+++ task-manager-back/index.js
@@ -2,6 +2,7 @@ import express from 'express'
 import dotenv from 'dotenv'
 import cors from 'cors'
 import { connectDB } from './db.js'
+import { Server, Socket } from 'socket.io'
 import userRoutes from './routes/userRoutes.js'
 import projectRoutes from './routes/projectRoutes.js'
 import taskRoutes from './routes/taskRoutes.js'
@@ -22,6 +23,18 @@ app.use(express.json())
 app.use('/api/user', userRoutes)
 app.use('/api/projects', projectRoutes)
 app.use('/api/task', taskRoutes)
-app.listen(3000, () => {
+const nodeServer = app.listen(3000, () => {
   console.log('Server listening on port 3000')
+})
+
+// socket io
+const io = new Server(nodeServer, {
+  pingTimeout: 60000,
+  cors: {
+    origin: process.env.FRONT_URL
+  }
+})
+io.on('connection', (socket) => {
+  console.log('conectado con socketio')
+  // definir eventos de socket io
 })
